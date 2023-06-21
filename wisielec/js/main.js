4cdaf19e249ab294f6b84e5400bf735cd016bfdf
Vyekk <s20699@pjwstk.edu.pl>
2023-06-21 16:04:09 +0200

    Refactored to singlequotes

--- wisielec/js/main.js
+++ wisielec/js/main.js
@@ -1,18 +1,17 @@
-import Hangman from "./Hangman.js";
+import Hangman from './Hangman.js';
 
-const gameContainer = document.querySelector(".game-container");
-const gameBoard = document.querySelector(".hangman-container");
-const passwordForm = document.querySelector(".password-form");
-const answerContainer = document.querySelector(".answer-container");
-const alphabetContainer = document.querySelector(".alphabet-container");
-let alphabet = new Set([..."aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"]);
+const gameContainer = document.querySelector('.game-container');
+const hangmanContainer = document.querySelector('.hangman-container');
+const passwordForm = document.querySelector('.password-form');
+const answerContainer = document.querySelector('.answer-container');
+const alphabetContainer = document.querySelector('.alphabet-container');
+let alphabet = new Set([...'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż']);
 let password;
-let answer = "";
+let answer = '';
 
 const startGame = (e) => {
   e.preventDefault();
   password = e.target.password.value.toLowerCase();
-  console.log([...password]);
   gameContainer.removeChild(passwordForm);
   createAnswerUi();
   createAlphabetUi();
@@ -29,30 +28,34 @@ const checkLetter = (e) => {
 
 const createAlphabetUi = () => {
   alphabet.forEach((letter) => {
-    const spanElement = document.createElement("button");
+    const spanElement = document.createElement('button');
     spanElement.innerText = letter;
-    const spaceElement = document.createTextNode(" ");
-    spanElement.addEventListener("click", checkLetter);
+    const spaceElement = document.createTextNode(' ');
+    spanElement.addEventListener('click', checkLetter);
     alphabetContainer.appendChild(spanElement);
     alphabetContainer.appendChild(spaceElement);
   });
 };
 
 const createAnswerUi = () => {
-  answerContainer.innerText = "";
-  let answerNode = "";
+  answerContainer.innerText = '';
+  let answerNode = '';
   [...password].forEach((passwordLetter) => {
     if (answer.includes(passwordLetter)) {
       answerNode += ` ${passwordLetter} `;
-    } else if (passwordLetter === " ") {
-      answerNode += "\u00A0\u00A0";
+    } else if (passwordLetter === ' ') {
+      answerNode += '\u00A0\u00A0';
     } else {
-      answerNode += " _ ";
+      answerNode += ' _ ';
     }
   });
   const answerDOMTextNode = document.createTextNode(answerNode);
   answerContainer.appendChild(answerDOMTextNode);
 };
 
-const hg = new Hangman();
-passwordForm.addEventListener("submit", startGame);
+const createHangmanUi = () => {
+  const canvasElement = document.createElement('canvas');
+};
+
+const hangman = new Hangman();
+passwordForm.addEventListener('submit', startGame);
