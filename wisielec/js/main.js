3276684a01425127da7ed6c61893da2a7113639d
Vyekk <s20699@pjwstk.edu.pl>
2023-06-21 04:12:19 +0200

    Improved function of writing answer

--- wisielec/js/main.js
+++ wisielec/js/main.js
@@ -7,21 +7,29 @@ const answerContainer = document.querySelector(".answer-container");
 const alphabetContainer = document.querySelector(".alphabet-container");
 let alphabet = new Set([..."aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"]);
 let password;
+let answer = "";
 
-const writePassword = (e) => {
+const startGame = (e) => {
   e.preventDefault();
   password = e.target.password.value;
+  console.log([...password]);
   gameContainer.removeChild(passwordForm);
+  createAnswerUi();
   createAlphabetUi();
 };
 
 const checkLetter = (e) => {
-  console.log(password.includes(e.target.innerText));
+  const checkingLetter = e.target.innerText;
+  e.target.disabled = true;
+  if (password.includes(checkingLetter)) {
+    answer += checkingLetter;
+  }
+  createAnswerUi();
 };
 
 const createAlphabetUi = () => {
   alphabet.forEach((letter) => {
-    const spanElement = document.createElement("span");
+    const spanElement = document.createElement("button");
     spanElement.innerText = letter;
     const spaceElement = document.createTextNode(" ");
     spanElement.addEventListener("click", checkLetter);
@@ -30,7 +38,21 @@ const createAlphabetUi = () => {
   });
 };
 
-const createAnswerUi = () => {};
+const createAnswerUi = () => {
+  answerContainer.innerText = "";
+  let answerNode = "";
+  [...password].forEach((passwordLetter) => {
+    if (answer.includes(passwordLetter)) {
+      answerNode += ` ${passwordLetter} `;
+    } else if (passwordLetter === " ") {
+      answerNode += "\u00A0\u00A0";
+    } else {
+      answerNode += " _ ";
+    }
+  });
+  const answerDOMTextNode = document.createTextNode(answerNode);
+  answerContainer.appendChild(answerDOMTextNode);
+};
 
 const hg = new Hangman();
-passwordForm.addEventListener("submit", writePassword);
+passwordForm.addEventListener("submit", startGame);
