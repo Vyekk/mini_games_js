ff328235b50115d91ea77d561c4990186f8fe8f7
Vyekk <s20699@pjwstk.edu.pl>
2023-06-21 03:33:37 +0200

    Added more funcionalities

--- wisielec/js/main.js
+++ wisielec/js/main.js
@@ -1,16 +1,36 @@
 import Hangman from "./Hangman.js";
 
-const gameContainer = document.querySelector('.container');
-const gameBoard = document.querySelector('.container-hangman');
-const passwordForm = document.querySelector('.password-form');
-
+const gameContainer = document.querySelector(".game-container");
+const gameBoard = document.querySelector(".hangman-container");
+const passwordForm = document.querySelector(".password-form");
+const answerContainer = document.querySelector(".answer-container");
+const alphabetContainer = document.querySelector(".alphabet-container");
+let alphabet = new Set([..."aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"]);
+let password;
 
 const writePassword = (e) => {
-    e.preventDefault();
-    console.log(e.target.password.value);
-    gameContainer.removeChild(passwordForm);
-
+  e.preventDefault();
+  password = e.target.password.value;
+  gameContainer.removeChild(passwordForm);
+  createAlphabetUi();
 };
 
+const checkLetter = (e) => {
+  console.log(password.includes(e.target.innerText));
+};
+
+const createAlphabetUi = () => {
+  alphabet.forEach((letter) => {
+    const spanElement = document.createElement("span");
+    spanElement.innerText = letter;
+    const spaceElement = document.createTextNode(" ");
+    spanElement.addEventListener("click", checkLetter);
+    alphabetContainer.appendChild(spanElement);
+    alphabetContainer.appendChild(spaceElement);
+  });
+};
+
+const createAnswerUi = () => {};
+
 const hg = new Hangman();
-passwordForm.addEventListener('submit', writePassword);
+passwordForm.addEventListener("submit", writePassword);
